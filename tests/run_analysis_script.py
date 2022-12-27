f387b6433343d18a1eb9b07415cec4001fa04714
Danny Leboff <31274819+Techatonic@users.noreply.github.com>
2022-12-27 14:30:45 +0000

    Add script for runtime analysis

---
+++ tests/run_analysis_script.py
@@ -0,0 +1,26 @@
+import os
+from argparse import ArgumentParser
+import time
+import numpy as np
+import pandas as pd
+
+parser = ArgumentParser('Analysis on Runtime of Event Scheduler')
+parser.add_argument("-n", required=True, type=int, help="Number of times to run the program")
+parser.add_argument("--stmt", required=True, type=str, help="statement to execute")
+args = None
+try:
+    args = parser.parse_args()
+except:
+    print("Invalid input")
+    exit()
+
+times = np.array([], dtype=float)
+
+for run in range(args.n):
+    start_time = time.time()
+    os.system(args.stmt)
+    times = np.append(times, time.time() - start_time)
+
+df = pd.DataFrame(times)
+print()
+print(df.describe().transpose())
